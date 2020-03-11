8f91a1783684bc0fcf09d48a6230bd06234b3169
andipahlevy <andilevi@gmail.com>
2020-03-11 11:08:51 +0700

    no message

--- resources/views/eks/parts/head.blade.php
+++ resources/views/eks/parts/head.blade.php
@@ -75,3 +75,13 @@
 		background-image: url({{ url('theme/images/newsletter.png') }});
 	}
 	</style>
+	
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-160267768-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-160267768-1');
+</script>
