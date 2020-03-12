0959f0b413c61a3a4d3fde4361f3dd2d3e8e4508
andipahlevy <andilevi@gmail.com>
2020-03-12 13:49:00 +0700

    no message

--- resources/views/eks/parts/style.blade.php
+++ resources/views/eks/parts/style.blade.php
@@ -37,5 +37,5 @@ a.slicknav_btn.slicknav_collapsed{
     padding: 10px;
     color: #fff !important;
 }
-.fa-facebook { background: #3B5998; color: white; } .fa-twitter { background: #55ACEE; color: white; } .fa-google { background: #dd4b39; color: white; } .fa-linkedin { background: #007bb5; color: white; }
+.fa-facebook { background: #3B5998; color: white; } .fa-twitter { background: #55ACEE; color: white; } .fa-whatsapp { background: #3db24c; color: white; } .fa-linkedin { background: #007bb5; color: white; }
 </style>
